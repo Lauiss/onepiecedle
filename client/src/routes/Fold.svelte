e85a53170dc3b58f165c8839c1e3a83b1dfb7c1e
Lucas <lucas@heintz.dev>
2023-12-05 20:41:19 +0100

    Homepage background, text and paper components

---
+++ client/src/routes/Fold.svelte
@@ -0,0 +1,21 @@
+<svg viewBox="0 0 38 96" preserveAspectRatio="none"  fill="none" xmlns="http://www.w3.org/2000/svg">
+<path preserveAspectRatio="none"  d="M26.5277 5.70543C26.5277 5.70543 31.7638 5.36434 32.5694 7.06977C33.3749 8.77519 20.8889 13.2093 8 17.3023C21.3176 21.9837 29.3472 69.5335 29.3472 92V92C33.5737 92 36.9999 88.5738 36.9999 84.3472V12C36.9999 7.58172 33.4182 4 28.9999 4H26.5277" stroke="#462A10" stroke-opacity="0.3"/>
+<g filter="url(#filter0_d_127_372)">
+<path preserveAspectRatio="none"  d="M36.9999 84.3472V12C36.9999 7.58172 33.4182 4 28.9999 4H26.5277V5.70543C26.5277 5.70543 31.7638 5.36434 32.5694 7.06977C33.3749 8.77519 20.8889 13.2093 8 17.3023C21.3176 21.9837 29.3472 69.5335 29.3472 92C33.5737 92 36.9999 88.5738 36.9999 84.3472Z" fill="url(#paint0_linear_127_372)"/>
+</g>
+<defs>
+<filter id="filter0_d_127_372" x="0" y="0" width="37" height="96" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dx="-4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_127_372"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_127_372" result="shape"/>
+</filter>
+<linearGradient id="paint0_linear_127_372" x1="22.5" y1="4" x2="22.5" y2="92" gradientUnits="userSpaceOnUse">
+<stop stop-opacity="0.01"/>
+<stop offset="0.604167" stop-opacity="0.1"/>
+</linearGradient>
+</defs>
+</svg>
---
+++ client/src/routes/Text.svelte
@@ -0,0 +1,81 @@
+<script>
+    export let as = 'p';
+    export let size = 1;
+
+    let sizeClass = `size-${size}`
+</script>
+
+<svelte:element this={as} class={sizeClass + " text"} >
+<slot></slot>
+</svelte:element>
+
+<style>
+
+    .size-1 {
+        font-size: 3rem;
+		text-shadow: 1px 1px 0 #000,
+                    -1px 1px 0 #000,
+                    1px -1px 0 #000,
+                    -1px -1px 0 #000,
+                    0px 1px 0 #000,
+                    0px -1px 0 #000,
+                    -1px 0px 0 #000,
+                    1px 0px 0 #000,
+                    2px 2px 0 #000,
+                    -2px 2px 0 #000,
+                    2px -2px 0 #000,
+                    -2px -2px 0 #000,
+                    0px 2px 0 #000,
+                    0px -2px 0 #000,
+                    -2px 0px 0 #000,
+                    2px 0px 0 #000,
+                    1px 2px 0 #000,
+                    -1px 2px 0 #000,
+                    1px -2px 0 #000,
+                    -1px -2px 0 #000,
+                    2px 1px 0 #000,
+                    -2px 1px 0 #000,
+                    2px -1px 0 #000,
+                    -2px -1px 0 #000;
+    }
+
+    .size-2 {
+        font-size: 2rem;
+		text-shadow: 1px 1px 0 #000,
+                    -1px 1px 0 #000,
+                    1px -1px 0 #000,
+                    -1px -1px 0 #000,
+                    0px 1px 0 #000,
+                    0px -1px 0 #000,
+                    -1px 0px 0 #000,
+                    1px 0px 0 #000,
+                    2px 2px 0 #000,
+                    -2px 2px 0 #000,
+                    2px -2px 0 #000,
+                    -2px -2px 0 #000,
+                    0px 2px 0 #000,
+                    0px -2px 0 #000,
+                    -2px 0px 0 #000,
+                    2px 0px 0 #000,
+                    1px 2px 0 #000,
+                    -1px 2px 0 #000,
+                    1px -2px 0 #000,
+                    -1px -2px 0 #000,
+                    2px 1px 0 #000,
+                    -2px 1px 0 #000,
+                    2px -1px 0 #000,
+                    -2px -1px 0 #000;
+    }
+
+    .size-3 {
+        font-size: 1.25rem;
+        -webkit-text-stroke: black;
+        -webkit-text-stroke-width: 1px;
+    }
+    
+	.text {
+		font-family: Luckiest Guy;
+		color: white;
+		text-transform: uppercase;
+	}
+</style>
